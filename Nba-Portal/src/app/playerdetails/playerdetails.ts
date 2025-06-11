eea1b937f6b65d63459ad3caed1fbfa3d19f75c3
LorenzErber <lorenzerber@gmail.com>
2025-06-11 14:27:23 +0200

    grundlagen

---
+++ Nba-Portal/src/app/playerdetails/playerdetails.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playerdetails',
+  imports: [],
+  templateUrl: './playerdetails.html',
+  styleUrl: './playerdetails.css'
+})
+export class Playerdetails {
+
+}
